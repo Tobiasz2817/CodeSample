1a85896d06bb2ec5c068d2334e60d67187158a7e
Tobiasz Mazurek <Tobiasz2817@outlook.com>
2024-09-20 10:36:39 +0200

    clear old samples

--- UGS/Lobby/Contains/LobbyContains.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameZone.Multiplayer.UGS.Lobby.LobbyContains
-{
-    public static class LobbyContains
-    {
-        public const string KEY_PLAYER_NAME = "PlayerName";
-        public const string KEY_PLAYER_ICON_INDEX = "PlayerIconIndex";
-        public const string KEY_LOBBY_CODE_JOIN = "LobbyJoinCode";
-    }
-}
