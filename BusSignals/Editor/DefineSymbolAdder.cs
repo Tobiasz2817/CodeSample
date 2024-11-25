b1e1b3cc3d000bfc5bb2c80028e31304e33e835b
Tobiasz Mazurek <Tobiasz2817@outlook.com>
2024-11-25 13:47:14 +0100

    Event/Signal bus update

---
+++ BusSignals/Editor/DefineSymbolAdder.cs
@@ -0,0 +1,14 @@
+﻿#if UNITY_EDITOR
+using CoreUtility;
+using UnityEditor;
+
+namespace BusSignals.Editor {
+    [InitializeOnLoad]
+    public class DefineSymbolAdder {
+        const string SignalsSymbol = "SIGNALS";
+        
+        static DefineSymbolAdder() =>
+            Utility.TryAddDefineSymbol(SignalsSymbol);
+    }
+}
+#endif
